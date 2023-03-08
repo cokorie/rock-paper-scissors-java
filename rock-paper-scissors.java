355a4cc8a6f3147c4ce83240ae5b3bc77142dfd5
cokorie <chijioke.okorie@gmail.com>
2023-03-08 17:43:01 -0600

    Add Valid Player Move

--- rock-paper-scissors.java
+++ rock-paper-scissors.java
@@ -5,7 +5,14 @@ public class RockPaperScissors {
         String computerMove = rps[new Random().nextInt(rps.length)];
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your move (r, p, or s)");
-        String playerMove = scanner.nextLine();
+        String playerMove;
+
+        while(true) {
+            System.out.println("Please enter your move (r, p, or s)");
+            playerMove = scanner.nextLine();
+            if(playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s"))
+                break;
+        }
+        System.out.println(playerMove + " is not a valid move.");
     }
 }
