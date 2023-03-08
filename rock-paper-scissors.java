a30a77e3c406ce1c30d36e8ca994f06f52ef9c83
cokorie <chijioke.okorie@gmail.com>
2023-03-08 16:53:29 -0600

    Add String Variables for Rock Paper Scissors and Movement

--- rock-paper-scissors.java
+++ rock-paper-scissors.java
@@ -1,6 +1,7 @@
 public class RockPaperScissors {
     public static void main(String[] args)
     {
-        
+        String rps = {"r", "p", "s"};
+        String computerMove = rps[new Random().nextInt(rps.length)];
     }
 }
