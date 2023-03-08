866480ca3cfccf0fc0f5b3a889be739113810df3
cokorie <chijioke.okorie@gmail.com>
2023-03-08 17:25:04 -0600

    Add Scanner Prompt and Initial Game Text

--- rock-paper-scissors.java
+++ rock-paper-scissors.java
@@ -3,5 +3,9 @@ public class RockPaperScissors {
     {
         String rps = {"r", "p", "s"};
         String computerMove = rps[new Random().nextInt(rps.length)];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your move (r, p, or s)");
+        String playerMove = scanner.nextLine();
     }
 }
