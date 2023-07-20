f014f1168372fe313221c02c812078df90125206
cokorie <chijioke.okorie@gmail.com>
2023-07-20 06:44:39 -0500

    add character to paper winning against rock message

--- rock-paper-scissors.java
+++ rock-paper-scissors.java
@@ -35,7 +35,7 @@ public class RockPaperScissors {
                 if (computerMove.equals("s")) {
                     System.out.println("You lose.");
                 } else if (computerMove.equals("r")) {
-                    System.out.println("You win");
+                    System.out.println("You win!");
                 }
             }
 
