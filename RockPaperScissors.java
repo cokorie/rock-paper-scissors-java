29b869a81a714c04c90c09d359def337ea47e266
cokorie <chijioke.okorie@gmail.com>
2024-08-01 10:22:22 -0500

    revert winning scissors-paper message to caps

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -43,7 +43,7 @@ public class RockPaperScissors {
                 if (computerMove.equals("r")) {
                     System.out.println("You lose!");
                 } else if (computerMove.equals("p")) {
-                    System.out.println("You WIN!");
+                    System.out.println("You win!");
                 }
             }
 
