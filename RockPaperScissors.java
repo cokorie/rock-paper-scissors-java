b2c8b4785a4758c4592f1b970b572222f5683eb5
cokorie <chijioke.okorie@gmail.com>
2024-08-06 16:23:27 -0500

    update R

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -40,7 +40,7 @@ public class RockPaperScissors {
             }
 
             else if (playerMove.equals("s")) {
-                if (computerMove.equals("r")) {
+                if (computerMove.equals("R")) {
                     System.out.println("You lose!");
                 } else if (computerMove.equals("p")) {
                     System.out.println("You win!");
