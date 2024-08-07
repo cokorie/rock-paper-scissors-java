af50ee1d3b7b95d2a0125590984b3978039a6b08
cokorie <chijioke.okorie@gmail.com>
2024-08-07 16:47:47 -0500

    capitalize Y response

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -47,7 +47,7 @@ public class RockPaperScissors {
                 }
             }
 
-            System.out.println("PLAY AGAIN? (y/n)");
+            System.out.println("PLAY AGAIN? (Y/n)");
             String playAgain = scanner.nextLine();
 
             if (!playAgain.equals("Y")) {
