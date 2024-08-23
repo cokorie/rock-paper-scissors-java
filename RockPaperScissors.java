4671879e1757f521a6a024e5e68fe7ac0e1bce89
cokorie <chijioke.okorie@gmail.com>
2024-08-23 11:46:03 -0500

    revert orig You win message in rock-scissors

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -27,7 +27,7 @@ public class RockPaperScissors {
                 if (computerMove.equals("p")) {
                     System.out.println("You lose!");
                 } else if (computerMove.equals("s")) {
-                    System.out.println("YOU WIN!");
+                    System.out.println("You win!");
                 }
             }
 
