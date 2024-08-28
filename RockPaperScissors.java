c4e0e783e78d17fb43a1aaf03a78e6cc18488655
cokorie <chijioke.okorie@gmail.com>
2024-08-28 09:32:49 -0500

    update rps to capital P

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -7,7 +7,7 @@ public class RockPaperScissors {
 
     Scanner scanner = new Scanner(System.in);
     while(true) {
-        String rps = {"r", "p", "s"};
+        String rps = {"r", "P", "s"};
         String computerMove = rps[new Random().nextInt(rps.length)];
 
         String playerMove;
