f935e35c5cfaa70c7f7aab28a5d0aee46a737c10
cokorie <chijioke.okorie@gmail.com>
2025-02-11 02:22:28 -0600

    revert to orig r for prompt

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -7,7 +7,7 @@ public class RockPaperScissors {
 
     Scanner scanner = new Scanner(System.in);
     while(true) {
-        String rps = {"R", "P", "S"};
+        String rps = {"r", "P", "S"};
         String computerMove = rps[new Random().nextInt(rps.length)];
 
         String playerMove;
