304c23eb6c40265966840de34a7bbdf2c5ca6e1a
cokorie <chijioke.okorie@gmail.com>
2025-05-25 23:50:57 -0500

    update rps

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -7,7 +7,7 @@ public class RockPaperScissors {
 
     Scanner scanner = new Scanner(System.in);
     while(true) {
-        String RPS = {"r", "p", "s"};
+        String rps = {"r", "p", "s"};
         String computerMove = rps[new Random().nextInt(rps.length)];
 
         String playerMove;
