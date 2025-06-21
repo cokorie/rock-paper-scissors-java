bdea7d3515719c2b0cfe19186998ea7cccd219cb
cokorie <chijioke.okorie@gmail.com>
2025-06-20 19:33:40 -0500

    cap win message

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -27,7 +27,7 @@ public class RockPaperScissors {
                 if (computerMove.equals("p")) {
                     System.out.println("You lose!");
                 } else if (computerMove.equals("s")) {
-                    System.out.println("You win!");
+                    System.out.println("You WIN!");
                 }
             }
 
