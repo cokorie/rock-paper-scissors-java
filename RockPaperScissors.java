b8d06a3629be6f208b0e14b3fc8d15ba3eb0acad
cokorie <chijioke.okorie@gmail.com>
2025-07-30 09:53:22 -0500

    orig s-r losing message

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -41,7 +41,7 @@ public class RockPaperScissors {
 
             else if (playerMove.equals("s")) {
                 if (computerMove.equals("r")) {
-                    System.out.println("You lost!");
+                    System.out.println("You lose!");
                 } else if (computerMove.equals("p")) {
                     System.out.println("You win!");
                 }
