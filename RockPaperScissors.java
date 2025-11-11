d187e3f58dc2f5c66785cc5ed6bc6b15e1716044
cokorie <chijioke.okorie@gmail.com>
2025-11-11 08:26:49 -0600

    lowercase y in play again message

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -47,7 +47,7 @@ public class RockPaperScissors {
                 }
             }
 
-            System.out.println("PLAY AGAIN? (Y/N)");
+            System.out.println("PLAY AGAIN? (y/N)");
             String playAgain = scanner.nextLine();
 
             if (!playAgain.equals("Y")) {
