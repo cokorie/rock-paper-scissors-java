d2f6f196dd53db880541ffb9ce4281e7b71a5ee6
cokorie <chijioke.okorie@gmail.com>
2026-01-08 20:17:15 -0600

    revert p-r message to win

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -35,7 +35,7 @@ public class RockPaperScissors {
                 if (computerMove.equals("S")) {
                     System.out.println("You lost!");
                 } else if (computerMove.equals("R")) {
-                    System.out.println("You won!");
+                    System.out.println("You win!");
                 }
             }
 
