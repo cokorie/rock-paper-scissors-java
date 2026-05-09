f686a58e28e89778618ed36e9a0e090dbc40ecc9
cokorie <chijioke.okorie@gmail.com>
2026-05-09 10:25:04 -0500

    add orig p-s lose message

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -33,7 +33,7 @@ public class RockPaperScissors {
 
             else if (playerMove.equals("p")) {
                 if (computerMove.equals("s")) {
-                    System.out.println("You lost!");
+                    System.out.println("You lose!");
                 } else if (computerMove.equals("r")) {
                     System.out.println("You win!");
                 }
