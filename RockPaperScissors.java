dc06fca919df786ba8e6b3f03f9936d5a02247e6
cokorie <chijioke.okorie@gmail.com>
2026-05-14 07:14:30 -0500

    add N to play again message

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -47,7 +47,7 @@ public class RockPaperScissors {
                 }
             }
 
-            System.out.println("PLAY AGAIN? (Y/n)");
+            System.out.println("PLAY AGAIN? (Y/N)");
             String playAgain = scanner.nextLine();
 
             if (!playAgain.equals("Y")) {
